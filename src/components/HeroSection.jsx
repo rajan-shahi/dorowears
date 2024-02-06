e8d2ca1cd0f6629d4c9686c0e7d585b9c92c74d8
rajan <rajanshahi1267@gmail.com>
2024-02-06 10:00:49 -0700

    add

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -36,18 +36,27 @@ const HeroSection = () => {
               key={index}
               className=" flex flex-col items-center justify-center"
             >
-              <img className=" relative object-cover" src={item.img} alt="" />
+              <img className=" md:h-full h-60 relative object-cover" src={item.img} alt="" />
               <div className=" md:w-5/12 absolute  md:mr-96 flex flex-col gap-4">
-                <span data-aos="fade-up"
-     data-aos-duration="1500" className=" md:text-4xl  text-gray-700 font-semibold tracking-widest">
+                <span
+                  data-aos="fade-up"
+                  data-aos-duration="1500"
+                  className=" md:text-4xl  text-gray-700 font-semibold tracking-widest"
+                >
                   {item.name}
                 </span>
-                <span data-aos="fade-up"
-     data-aos-duration="2000" className=" md:flex hidden tracking-widest text-xl text-gray-600">
+                <span
+                  data-aos="fade-up"
+                  data-aos-duration="2000"
+                  className=" md:flex hidden tracking-widest text-xl text-gray-600"
+                >
                   {item.desc}
                 </span>
-                <button data-aos="fade-up"
-     data-aos-duration="2500" className=" flex  hover:bg-primary-300 duration-500  bg-primary-350 w-max md:px-10 md:py-2 py-1 px-5 md:text-md text-sm text-gray-100">
+                <button
+                  data-aos="fade-up"
+                  data-aos-duration="2500"
+                  className=" flex  hover:bg-primary-300 duration-500  bg-primary-350 w-max md:px-10 md:py-2 py-1 px-5 md:text-md text-sm text-gray-100"
+                >
                   {item.button}
                 </button>
               </div>
